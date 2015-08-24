f795eaafda4b1ecf6486829e735aa9236703e2bb
Kent Collins <kent.r.collins@gmail.com>
2015-08-24 06:36:20 +0100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 /**
 /  Write a main method that prints "Hello, world" to the screen.
+/
+/  Compile this file from the command shell (bash)
+/  $ javac Hello.java 
+/
+/  Run the resulting class from the command shell (bash)
+/  $ java Hello
 */
 
 public class Hello {
